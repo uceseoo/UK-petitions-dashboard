44ece5e7f4d877f78211745d68272be43cf26074
uceseoo <uceseoo@ucl.ac.uk>
2022-08-27 02:00:57 +0100

    add loading state for pie charts; add constituency boundaries json for the map

---
+++ server/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+import petitionRoute from "./routes/petition.js";
+
+dotenv.config();
+
+const {PORT, MONGODB_URI} = process.env;
+
+const app = express();
+
+app.use(express.json({limit: "30mb", extended: true}));
+app.use(express.urlencoded({limit: "30mb", extended: true}));
+
+app.use(cors());
+
+app.use(express.Router());
+app.use("/petition", petitionRoute);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to UK Petitions API");
+});
+
+const SERVER_PORT = PORT || 5000;
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Application is connected to MongoDB...");
+
+    app.listen(SERVER_PORT, () =>
+      console.log(`Server running on port: ${SERVER_PORT}...`)
+    );
+  })
+  .catch(err => {
+    console.log(err);
+  });
