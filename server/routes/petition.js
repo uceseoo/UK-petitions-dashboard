dff2d012627c2c4d075304218110e198ffadbedc
uceseoo <uceseoo@ucl.ac.uk>
2022-08-27 01:57:06 +0100

    add fetch data to header component; add interactivity- connect petition state filter to pie charts grouped by themes; add fetchPetition route that returns petition from server

--- server/routes/petition.js
+++ server/routes/petition.js
@@ -2,20 +2,25 @@ import express from "express";
 
 const router = express.Router();
 
-import {addPetition} from "../controllers/addPetition.js";
 import {fetchMapData} from "../controllers/fetchMapData.js";
-import {fetchPetitions} from "../controllers/fetchPetitions.js";
 import {fetchBarGraphData} from "../controllers/barGraphData.js";
 import {fetchLineChartData} from "../controllers/lineChartData.js";
-
-router.get("/add", fetchMapData);
+import {
+  groupedByDepartment,
+  groupedByState,
+} from "../controllers/fetchPieChartData.js";
+import {fetchPetitions} from "../controllers/fetchPetitions.js";
 
 router.get("/get/map/geojson/:topic", fetchMapData);
 
-router.post("/fetch/petitions", fetchPetitions);
+router.post("/fetch/pie-chart/data/state", groupedByState);
+
+router.post("/fetch/pie-chart/data/department", groupedByDepartment);
 
 router.post("/fetch/bar-graph/data", fetchBarGraphData);
 
 router.get("/fetch/line-chart/data", fetchLineChartData);
+
+router.post("/fetch/petitions", fetchPetitions);
 
 export default router;
