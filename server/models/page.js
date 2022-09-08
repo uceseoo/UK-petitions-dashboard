b58c80f4e0eff96de920d9a09268f67da1033657
uceseoo <uceseoo@ucl.ac.uk>
2022-09-08 23:29:02 +0100

    implement last update button

--- server/models/page.js
+++ server/models/page.js
@@ -3,6 +3,7 @@ import moment from "moment";
 
 const pageSchema = mongoose.Schema({
   last_page: String,
+  last_page_number: Number,
   updated_at: {
     type: Date,
     default: moment().format(),
